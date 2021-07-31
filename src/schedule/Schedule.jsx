7424fdc60fd830c037b6134221729c07fe4e6c56
Lucy Hao <hao.lucyy@gmail.com>
2021-07-31 09:07:47 -0600

    add careosel

--- src/schedule/Schedule.jsx
+++ src/schedule/Schedule.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import '../App.scss'
+import Carousel from 'react-elastic-carousel';
+
+const items = [
+    { id: 1, title: 'item #1' },
+    { id: 2, title: 'item #2' },
+    { id: 3, title: 'item #3' },
+    { id: 4, title: 'item #4' },
+    { id: 5, title: 'item #5' }
+]
 
 const Schedule = props =>
     <div className="schedule">
         <h2>
             {props.title}
         </h2>
+        <Carousel>
+            {items.map(item => <div key={item.id}>{item.title}</div>)}
+        </Carousel>
     </div>
 
 Schedule.propTypes = {
