25f092882df1288ea49160ec98341168713017af
Lucy Hao <hao.lucyy@gmail.com>
2021-08-21 18:08:10 -0600

    add home link

--- src/Sponsorship.jsx
+++ src/Sponsorship.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sponsorship = () => {
     return (
         <div className="sponsorship">
-            hi
+            <Link to="/site/">go home</Link>
         </div>
     );
 };
