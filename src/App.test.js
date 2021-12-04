1d958e9fd15fb83177943cfad66b2585f9ec2318
Lucy Hao <hao.lucyy@gmail.com>
2021-12-04 15:54:36 -0800

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import About from './about/About';
 import React from 'react';
-import * as Constants from './constants';
 
 test('renders learn react link', () => {
-    render(<About title={Constants.about} />);
-    const linkElement = screen.getByText(Constants.about);
+    render(<About title={'ABOUT'} />);
+    const linkElement = screen.getByText('ABOUT');
     expect(linkElement).toBeInTheDocument();
 });
